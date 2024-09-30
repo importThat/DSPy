8bfc4c4dcc78612cbfeada0aabaa02fa005cb422
importThat <165555972+importThat@users.noreply.github.com>
2024-09-30 14:54:56 +1000

    changed to work with alpha launch and with new signal structure

--- Examples/CPFSK_example.py
+++ Examples/CPFSK_example.py
@@ -6,25 +6,15 @@ instantaneous phase transitions
 """
 
 # Intermediate Hz, This will be the highest frequency that will occur in the modulated wave
-F = 2000
+f = 2000
+sps = 32    # samples per symbol
+fs = 8000   # sampling rate. I'm going to use a high fs here so the CPFSK is easier to see
 
 # Our message in symbol form. In this example we have 4 symbols, so each symbol would typically represent 2 bits
-MESSAGE = dsproc.utils.create_message(5000, 8)
-
-SYMBOL_RATE = 500      # Symbols per second
-DUR = len(MESSAGE) / SYMBOL_RATE    # Message duration (in seconds)
-
-# We want the sampling rate to satisfy Nyquist's level (2* highest frequency) and to also be an integer
-# multiple of the symbol rate (for ease of use reasons)
-Fs = SYMBOL_RATE
-while Fs <= F * 2:
-    Fs += SYMBOL_RATE
-
-# Increase the sample rate a bit so the CPFSK is easier to see
-Fs += SYMBOL_RATE * 100
+MESSAGE = dsproc.create_message(5000, 8)
 
 # Create the sig object with the given params
-s = dsproc.Mod(message=MESSAGE, f=F, fs=Fs, duration=DUR, amplitude=1)
+s = dsproc.Mod(message=MESSAGE, fs=fs, sps=16, f=f, amplitude=1)
 
 # Apply the frequency shift keying
 s.CPFSK()
@@ -32,6 +22,7 @@ s.CPFSK()
 # CPFSK uses a phase offset to smooth the transitions between frequency shifts. You can see this in the time
 # graph
 s.time(n=s.sps*10)
+
 # The fft is also a bit cleaner than the fsk we looked at earlier
 s.fft()
 
@@ -42,4 +33,4 @@ s.baseband()
 s.specgram(nfft=1024)
 
 # Saves the samples as complex64 (compatible with gnuradio/usrp)
-s.save("CPFSK_test")
+#s.save("CPFSK_test")
