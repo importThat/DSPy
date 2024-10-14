0ce688bacdb0af1fef4a48e756f548b2354f3e5c
importThat <165555972+importThat@users.noreply.github.com>
2024-10-14 13:28:19 +1100

    Changed demod_fsk so it always associates the same bits with the same symbols
    added functions to find a window in an xmit, search for a dominant frequency, and window the signal based on amplitude

--- src/dsproc/sig/demod.py
+++ src/dsproc/sig/demod.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 from ._sig import Signal
 from scipy.cluster.vq import kmeans
 from .constellation import Constellation
+from scipy.signal import correlate, savgol_filter
 
 """
 Class with demod functions, ideally this is automated but very much still a work in progress
@@ -180,7 +181,6 @@ class Demod(Signal):
         levels = clusters[0]
 
         # Now we map the levels to symbols
-
         symbols = np.arange(len(levels))
         out = []
 
@@ -211,7 +211,11 @@ class Demod(Signal):
         for i in range(len(freq)):
             # if it's the first symbol of the sample
             if (i + 1) % sps == 0:
-                freq[i] = freq[i + 1]
+                # If it's the last sample
+                if i == len(freq)-1:
+                    continue
+                else:
+                    freq[i] = freq[i + 1]
 
         # Changes peaks into the average
         sd = np.std(freq)
@@ -238,7 +242,7 @@ class Demod(Signal):
         # Now we just cluster and then categorize
 
         clusters = kmeans(averaged, m, iter=iterations, check_finite=False)
-        levels = clusters[0]
+        levels = np.sort(clusters[0])
 
         symbols = np.arange(len(levels))
         out = []
@@ -248,6 +252,160 @@ class Demod(Signal):
             out.append(symbols[index])
 
         return np.array(out)
+
+    def transmit_window(self, min_amp: float, min_dur: int):
+        """
+        For use on recordings with signal pulses. Returns tuples which show the start and stop of each pulse.
+
+        min_amp: The minimum amplitude that consitutes a pulse. Masks out parts of the signal that are less than this.
+        min_dur: The minimum duration in samples for a pulse. Anything shorter than this will be dropped.
+
+        return a two-dimensional array where the first index is the start of a pulse (in samples) and the second is
+        the end (not-inclusive).
+        """
+        # Create an amplitude mask
+        amp_mask = np.abs(self.samples)
+
+        # If it's below it's noise, if it's above it's signal
+        amp_mask[amp_mask < min_amp] *= 0
+        amp_mask[amp_mask >= min_amp] = 1
+
+        # Figure out xmit durations
+        # Now we need to figure out the transmit durations, this is how long (in samples) each xmit lasts for
+        change_indices = np.where(amp_mask[:-1] != amp_mask[1:])[0] + 1
+
+        # So if the final index is on we need to add an off, but if it is off we don't need to add anything
+        # TODO
+        #  I feel like there is a bug here. My spidey senses are a-tinglin'
+        if amp_mask[-1] == 1:
+            change_indices = np.concatenate([change_indices, np.array([len(self.samples)])])
+
+        # If the first index is on, we need to add in a change at zero
+        if amp_mask[0] == 1:
+            change_indices = np.concatenate([np.array([0]), change_indices])
+
+        change_indices = change_indices.reshape((-1, 2))
+        durations = change_indices[:, 1] - change_indices[:, 0]
+        starts = change_indices[:, 0]
+
+        # Find the valid xmits
+        valid_xmits = np.where(durations >= min_dur)
+        xmit_tups = np.vstack([starts[valid_xmits], starts[valid_xmits] + durations[valid_xmits]]).T
+
+        return xmit_tups
+
+    def find_header(self, header: np.ndarray, signal: np.ndarray):
+        """
+        Uses correlation to find where the header occurs in the given signal. The header and the signal are normalised,
+        however large value differences can still skew results. Note that if no header is present, this function will
+        still return a result.
+
+        header: the header as a 1 dimensional array in the given domain
+        signal: A 1 dimensional array containing the header, needs to be in the appropriate domain.
+
+        return: Int, the sample number at which the header begins in the signal (the max value of the correlation)
+        """
+
+        norm_header = (header - np.mean(header)) / (np.std(header) * len(header))
+        norm_signal = (signal - np.mean(signal)) / (np.std(signal))
+
+        c = correlate(norm_signal, norm_header, mode='full')
+        xmit_start = np.argmax(c) - len(header) + 1
+
+        return xmit_start
+
+    def freq_search(self, start: int, end: int, bandwidth: int, tuning_steps: int = 30, fft_smoothness: int = 3):
+        """
+        Searches a sample range (start:end) for the dominant frequency that fits within the given bandwidth. First
+        computes an fft, then smooths that fft, then slides a window of size bandwidth over the fft to identify the
+        strongest frequency band. After it identifies this area, the function performs some tuning to narrow in on
+        the best spot.
+
+        Returns the centre-point of the window.
+
+        start: The index of the start of the sample range
+        end: The index of the end of that sample range
+        bandwidth: The expected bandwidth of the peak that you are looking for. Can be approximate
+        tuning_steps: How many iterations the tuning algorithm should be run for. More steps have exponentially less
+        effect
+        fft_smoothness: How much to smooth the fft by. 1 is max smoothness, and the length of the sample range is min
+        smoothness.
+        """
+        # Create the fft
+        cut = self.samples[start:end]
+        cut_fft = np.fft.fftshift(np.abs(np.fft.fft(cut)))
+        smooth_fft = savgol_filter(cut_fft, window_length=int(len(cut_fft) / fft_smoothness), polyorder=3)
+        cut_f_axis = np.arange(self.fs / -2, self.fs / 2, self.fs / len(cut))
+
+        # Convert bandwidth to fft indices
+        bw_per_bin = self.fs / len(smooth_fft)
+        bw = int(bandwidth / bw_per_bin)
+
+        # Figure out search parameters
+        step_size = int(len(smooth_fft)/10)
+        if not step_size:
+            step_size = 1
+            raise Warning("fft length is very small, consider using a larger sample window")
+
+        # Search over the freq axis and store the results
+        measurements = []
+
+        for j in range(0, len(smooth_fft), step_size):
+            fft_sum = sum(smooth_fft[j:j + bw])
+            amp_normed = fft_sum / bw
+            measurements.append(amp_normed)
+
+        # find the best measurements
+        measurements = np.array(measurements)
+        best_indices = np.where(measurements == max(measurements))[0]
+
+        # There might be multiple indices
+        # We always want to pick an index from the middle of the list
+        if len(best_indices) == 1:
+            best_index = best_indices
+
+        # If it's an even number of values long
+        elif len(best_indices) % 2 == 0:
+            # Randomly get one of the middle indices
+            index = len(best_indices) / 2 + np.random.randint(0, 2)
+            best_index = best_indices[index]
+
+        # If it's an odd number
+        elif len(best_indices) % 2 == 1:
+            # Get the middle index
+            index = np.ceil(len(best_indices) / 2)
+            best_index = best_indices[index]
+
+        else:
+            raise RuntimeError("Cannot find best index")
+
+        midpoint = best_index * step_size + int(bw / 2)
+        midpoint = midpoint[0]
+        # Now we have an approximate midpoint we can test it by testing the upper and lower sums
+        for i in range(tuning_steps):
+            tuning_step_size = int(bw / (4 + i))
+
+            upper_sum = sum(smooth_fft[midpoint:midpoint + int(bw / 2)])
+            lower_sum = sum(smooth_fft[midpoint:midpoint + int(bw / 2)])
+
+            # Step up
+            if upper_sum > lower_sum:
+                midpoint += tuning_step_size
+            # Step down
+            elif upper_sum < lower_sum:
+                midpoint -= tuning_step_size
+            # Perfection...?
+            else:
+                break
+
+        return cut_f_axis[midpoint]
+
+
+
+
+
+
+
 
 
 
