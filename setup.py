961298303f33b4219252b14091f76ce24131778f
importThat <165555972+importThat@users.noreply.github.com>
2024-10-14 13:28:39 +1100

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "dsproc",
-    version = "0.0.0a3",
+    version = "0.0.0a5",
     install_requires=["matplotlib", "numpy", "scipy"],
     description="dsproc: a powerful digital signals processing toolkit",
     long_description="dsproc is a Python package that enables the analysis and processing of digital radio signals using "
