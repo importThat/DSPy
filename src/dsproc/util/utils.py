dc61c14e427c1d2fd3feecbc453eb4dc5f316218
importThat <165555972+importThat@users.noreply.github.com>
2024-12-09 08:00:12 +1100

    Added docstrings and parameters

--- src/dsproc/util/utils.py
+++ src/dsproc/util/utils.py
@@ -1,9 +1,17 @@
+"""
+Utility functions for other dsproc classes
+"""
 import numpy as np
 
 
-def create_message(n=1000, m=50):
-    if n < m:
-        n = m
+def create_message(n: int = 1000, m: int = 50) -> np.ndarray:
+    """
+    Randomly generates a numpy array of integers of length n with m unique values.
+    :param n: The length of the message
+    :param m: How many unique values there are in the message
+    :return: A np array of values
+    """
+    n = max(n, m)
 
     out = np.arange(0, m)
 
@@ -14,17 +22,33 @@ def create_message(n=1000, m=50):
     return out
 
 
-def AWGN(n, power=0.01):
+def AWGN(n: int, power: float = 0.01) -> np.ndarray:
+    """
+    Returns a np array of complex additive white gaussian noise of length n. Use the power argument to scale the
+    amplitude of the noise
+    :param n: The length of the output noise
+    :param power: Scaling factor of the power
+    :return: A np array of complex 64 floats with length n
+    """
     # Create the noise
     n = (np.random.randn(n) + 1j * np.random.randn(n)) / np.sqrt(2)  # AWGN with unity power
     n = n.astype(np.complex64)
     # Scale it
     n = n * np.sqrt(power)
+    # Make sure its 64 bit
+    n = n.astype(np.complex64)
 
     return n
 
 
-def moving_average(x, n, weights=None):
+def moving_average(x: np.ndarray, n: int, weights: np.ndarray = None) -> np.ndarray:
+    """
+    Runs a moving average of length n over the data. Can use custom weights or otherwise the weights are set to one.
+    Custom weights must be a list or np array of the same length as n
+    :param x: np array of your input data (1d)
+    :param n: the length of the averaging window
+    :param weights: np array of floats, the moving average weights
+    """
     if weights is None:
         window = np.ones(n)
     else:
@@ -35,7 +59,10 @@ def moving_average(x, n, weights=None):
 
 def markify(symbols):
     """
-    Given some symbols returns an array of the pattern of the symbol occurences
+    Given some symbols returns an array of the pattern of the symbol occurrences
+
+    :param symbols: An array of ints
+    :return: Array of occurrences of those symbols
     """
     index = np.arange(len(symbols))
     output = None
@@ -50,7 +77,15 @@ def markify(symbols):
     return output
 
 
-def create_wave(t, f, amp, phase):
+def create_wave(t, f, amp, phase) -> np.ndarray:
+    """
+    Creates a complex wave from the given paramaters.
+    :param t: A np array of times
+    :param f: frequency, int
+    :param amp: amplitude, float
+    :param phase: phase in radians
+    :return: Array of complex 64 floats with length same as t
+    """
     angle = 2 * np.pi * f * t + phase
     wave = amp * np.cos(angle) + 1j * amp * np.sin(angle)
 
